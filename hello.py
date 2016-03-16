9e26216cb9971851cefdac88f981cbbcd64657a5
Viacheslav Shaposhnikov <shaposhnikov89@gmail.com>
2016-03-16 23:47:19 +0600

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def wsgi_application(environ, start_response):
+  start_response = ('200 OK', [('Content-Type', 'text/plain')])
+  queryString = environ['QUERY_STRING'].split('&')
+  responseBody = ''
+  for i in queryString:
+  	responseBody += i + '\n'
+  return [ responseBody ]
