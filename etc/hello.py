bf5fb76168cfac1da6abb8dd1c2ab84587f6e1ae
mr.shaposhnikov <mr.shaposhnikov@mail.ru>
2016-03-16 18:53:14 +0000

    all is correct...wtf

--- etc/hello.py
+++ etc/hello.py
@@ -6,6 +6,9 @@
 #    start_response(status, response_headers)
 #    options = environ['QUERY_STRING'].split("&")
 #    return [i+'\n' for i in options]
+
+bind = '0.0.0.0:8080'
+
 def app (environ,start_response):
     status = '200 OK'
     headers= [('Content-type','text/plain')]
