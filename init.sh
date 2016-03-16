3f85054e8483079825d78972d190d53b2e0d14bf
Viacheslav Shaposhnikov <shaposhnikov89@gmail.com>
2016-03-17 00:07:08 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart -b 0.0.0.0:8080 hello:app
-gunicorn -b 0.0.0.0:8080 hello  &
+gunicorn -b 0.0.0.0:8080  &
